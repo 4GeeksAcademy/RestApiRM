26d7a82d297c201cf8aa31348621da6213459d34
Rosangel593 <148271323+Rosangel593@users.noreply.github.com>
2024-04-12 20:09:41 +0000

    ready

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planet, Favorite
 #from models import Person
 
 app = Flask(__name__)
@@ -31,16 +31,84 @@ setup_admin(app)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
+
+current_logged_user_id = 1
+
 # generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_users():
+
+    users = User.query.all()
+
+    user_list = [element.serialize() for element in users]
+
+    return jsonify(user_list), 200
+
+@app.route('/planet', methods=['GET'])
+def get_planets():
+    allPlanets = Planet.query.all()
+    result = [element.serialize() for element in allPlanets]
+    return jsonify(result), 200
+
+@app.route('/planet-galaxy', methods=['GET'])
+def get_relation_planet_galaxy():
+    planets = Planet.query.all()
+    response = []
+    for planet in planets:
+        response.append({
+            'planet': planet.name,
+            'galaxy_id': planet.galaxy_id,
+            'galaxy_name': planet.galaxy.name
+        })
+    return jsonify(response)
+
+@app.route('/favorite/planet/<int:planet_id>', methods=['POST'])
+def add_favorite_planet(planet_id):
+    # Capturamos la informacion del request body y accedemos a planet_ud id
+ 
+    user = User.query.get(current_logged_user_id)
+
+    new_favorite = Favorite(user_id=current_logged_user_id, planet_id=planet_id)
+    db.session.add(new_favorite)
+    db.session.commit()
 
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "msg": "Favorito agregado correctamente", 
+        "favorite": new_favorite.serialize()
+    }
+
+    return jsonify(response_body), 200
+
+
+@app.route('/planet', methods=['POST'])
+def post_planet():
+
+    data = request.get_json()
+
+  
+    planet = Planet(name=data['name'], description=data['description'], population=data['population'])
+
+   
+    db.session.add(planet)
+    db.session.commit()
+
+    response_body = {"msg": "Planet inserted successfully"}
+    return jsonify(response_body), 200
+
+@app.route('/users/favorites', methods=['GET'])
+def get_user_favorites():
+    
+    user = User.query.get(current_logged_user_id)
+    favorites = user.favorites
+    serialized_favorites = [f.serialize() for f in favorites]
+
+    response_body = {
+        "msg": f"Aqui tienes los favoritos de {user.email}",
+        "favorites": serialized_favorites
     }
 
     return jsonify(response_body), 200
--- src/models.py
+++ src/models.py
@@ -2,11 +2,20 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class Galaxy(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    coordinate_center_x = db.Column(db.Float, nullable=False) 
+    coordinate_center_y = db.Column(db.Float, nullable=False)
+    
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+
+    favorites = db.relationship('Favorite')
+
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -16,4 +25,45 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    description = db.Column(db.String(240))
+    population = db.Column(db.Integer, default=0)
+
+    galaxy_id = db.Column(db.Integer, db.ForeignKey('galaxy.id'), nullable=False)
+
+    
+    galaxy = db.relationship('Galaxy')
+
+    def __repr__(self):
+        return f'<Favorite {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "population": self.population,
+        }
+
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'), nullable=True)
+   
+
+    planet = db.relationship('Planet')
+
+    def __repr__(self):
+        return f'<Favorite {self.id}>'
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'planet': self.planet.serialize() if self.planet else None,
         }
